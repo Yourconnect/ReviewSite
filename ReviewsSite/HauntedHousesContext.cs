df93a7f97180540ea72f65aab285c285dfd6999a
Naziyah <ziyahfolmar@gmail.com>
2021-10-06 14:51:15 -0700

    Added Nuget Packages

---
+++ ReviewsSite/HauntedHousesContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewsSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite
+{
+    public class HauntedHousesContext : DbContext
+    {
+        public DbSet<HauntedHouse> HauntedHouses { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb; Database=HauntedHouseDB_100621; Trusted_Connection=True";
+            optionsBuilder.UseSqlServer(connectionString).UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+
+        }
+
+
+    }
+
+
+}
--- ReviewsSite/Program.cs
+++ ReviewsSite/Program.cs
@@ -6,8 +6,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
 
-//Dev branch created
+
+
 namespace ReviewsSite
 {
     public class Program
