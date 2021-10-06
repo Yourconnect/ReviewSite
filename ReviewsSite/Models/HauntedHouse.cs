78d2e3955431fb16b3a1fcb46f663bab84bb87f0
JBroCrowe <jaxxoncrowe@gmail.com>
2021-10-06 11:38:48 -0400

    set up models, views, controllers, properties, etc

--- ReviewsSite/Models/HauntedHouse.cs
+++ ReviewsSite/Models/HauntedHouse.cs
@@ -7,5 +7,18 @@ namespace ReviewsSite.Models
 {
     public class HauntedHouse
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Duration { get; set; }
+        public string Location { get; set; }
+        public bool HandicapAccessible { get; set; }
+        public virtual List<Review> Reviews { get; set; }
+
+
+
+
     }
+
+
 }
--- ReviewsSite/Models/Review.cs
+++ ReviewsSite/Models/Review.cs
@@ -5,7 +5,16 @@ using System.Threading.Tasks;
 
 namespace ReviewsSite.Models
 {
-    public class Reviews
+    public class Review
     {
+        public int Id { get; set; }
+        public int HauntedHouseId { get; set; }
+        public virtual HauntedHouse HauntedHouse { get; set; }
+        public int Scareablity { get; set;}
+        public bool Recommend { get; set; }
+        public bool WouldReturn { get; set; }
+        public int StarRating { get; set; }
+        public string Comment { get; set; }
+        public string Author { get; set; }
     }
 }
