15e0c37e8a4d2581a43a3040f5aa384fb2d07adf
DawnaNatale <DawnaNatale1993@gmail.com>
2021-10-07 15:09:45 -0700

    AddedHauntedHouseIndexEtc

---
+++ ReviewsSite/Controllers/HauntedHouseController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReviewsSite.Models;
+using ReviewsSite.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReviewsSite.Controllers
+{
+    public class HauntedHouseController : Controller
+    {
+        private IRepository<HauntedHouse> _HauntedHouseRepo;
+
+        public HauntedHouseController(IRepository<HauntedHouse> HauntedHouseRepo)
+        {
+            this._HauntedHouseRepo = HauntedHouseRepo;
+        }
+        // GET: HauntedHouseController
+        public ActionResult Index()
+        {
+            return View(_HauntedHouseRepo.GetAll());
+        }
+
+        // GET: HauntedHouseController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_HauntedHouseRepo.GetByID(id));
+        }
+
+        // GET: HauntedHouseController/Create
+        public IActionResult AddHauntedHouse()
+        {
+            return View(new HauntedHouse());
+        }
+
+        [HttpPost]
+        public IActionResult AddHauntedHouse(HauntedHouse newHauntedHouse)
+        {
+            if (!String.IsNullOrEmpty(newHauntedHouse.Name))
+            {
+                _HauntedHouseRepo.Create(newHauntedHouse);
+                ViewBag.Result = "You have successfully added a haunted house!";
+            }
+            else
+            {
+                ViewBag.Error = "Name is required, please try again.";
+            }
+
+            return View(newHauntedHouse);
+        }
+
+        // POST: HauntedHouseController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: HauntedHouseController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HauntedHouseController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HauntedHouseController/Delete/5
+        public ActionResult DeleteHauntedHouse(int id)
+        {
+            var HauntedHouseToDelete = _HauntedHouseRepo.GetByID(id);
+            _HauntedHouseRepo.Delete(HauntedHouseToDelete);
+            return RedirectToAction("Index");
+        }
+
+        //// POST: HauntedHouseController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
--- ReviewsSite/Repositories/HauntedHouseRepository.cs
+++ ReviewsSite/Repositories/HauntedHouseRepository.cs
@@ -1,11 +1,54 @@
-﻿using System;
+﻿using ReviewsSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ReviewsSite.Repositories
 {
-    public class HauntedHouseRepository
+    public class HauntedHouseRepository : IRepository<HauntedHouse>
     {
+        public HauntedHousesContext db;
+
+        public HauntedHouseRepository(HauntedHousesContext db)
+        {
+            this.db = db;
+        }
+
+        public void Create(HauntedHouse obj)
+        {
+
+            db.HauntedHouses.Add(obj);
+            db.SaveChanges();
+        }
+        // Read
+        public IEnumerable<HauntedHouse> GetAll()
+        {
+            return db.HauntedHouses.ToList();
+        }
+
+        public HauntedHouse GetByID(int id)
+        {
+            return db.HauntedHouses.Find(id);
+            
+        }
+        // Update
+        public void Update(HauntedHouse obj)
+        {
+            db.HauntedHouses.Update(obj);
+            db.SaveChanges();
+        }
+
+        // Delete
+        public void Delete(HauntedHouse obj)
+        {
+            db.HauntedHouses.Remove(obj);
+            db.SaveChanges();
+        }
+
+        //public IEnumerable<Room> GetAllRooms()
+        //{
+        //    return db.Rooms.ToList();
+        //}
     }
 }
